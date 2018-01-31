bea3996710cb452c87d60457166bf7c0e4c12d39
Mykola Bilochub <nbelochub@gmail.com>
2018-01-31 15:52:04 +0200

    lib: add utility for call messages resending
    Callbacks passed to jstp method calls will now return an error when the
    connection is being closed.
    Since it is known, that callback messages are not being resent on connection
    drop, a special method on Connection was added that resends call message on
    connection to the server, and thus can be used for making a call to idempotent
    methods on the server.
    
    Co-authored-by: Dmytro Nechai <nechaido@gmail.com>

---
+++ test/node/resendable-call-from-client.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const test = require('tap');
+
+const jstp = require('../..');
+
+const appName = 'application';
+const interfaces = {
+  iface: {
+    method: (connection, callback) => {
+      callback(null);
+    },
+  },
+};
+
+const application = new jstp.Application(appName, interfaces);
+const serverConfig = { applications: [application] };
+
+const client = { session: null };
+
+const server = jstp.net.createServer(serverConfig);
+server.listen(0, () => {
+  const port = server.address().port;
+  jstp.net.connect(appName, null, port, 'localhost', (error, connection) => {
+    test.assertNot(error, 'must connect to server');
+
+    server.getClientsArray()[0].close();
+    connection.callMethodWithResend('iface', 'method', [], (error) => {
+      test.assertNot(error, 'must not return an error');
+      connection.close();
+      server.close();
+      test.end();
+    });
+
+    const port = server.address().port;
+    client.session = connection.session;
+
+    jstp.net.connect(appName, client, port, 'localhost', (error, conn) => {
+      test.assertNot(error, 'must reconnect to server');
+      connection = conn;
+    });
+  });
+});
